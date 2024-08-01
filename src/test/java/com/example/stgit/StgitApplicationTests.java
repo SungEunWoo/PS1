df36003b7a96e67454f2e7b81040f2e19f147028
SungEunWoo <sungeu18@naver.com>
2024-08-02 03:34:13 +0900

    first

---
+++ src/test/java/com/example/stgit/StgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
